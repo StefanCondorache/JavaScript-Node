122b87cc89349863e3b9de10b531ccfa01716765
StefanCondorache <stefan.condorache@gmail.com>
2024-12-01 16:30:53 +0200

    Changed main

--- NodeJSProjects/TaskManager/main.js
+++ NodeJSProjects/TaskManager/main.js
@@ -3,22 +3,28 @@ const app = express();
 const tasks = require('./NodeJSProjects/TaskManager/Routes/tasks.js')
 const connectDB = require('./NodeJSProjects/TaskManager/DB/connect.js');
 require('dotenv').config({ path: 'data.env' });
+const notFound = require('./NodeJSProjects/TaskManager/Middleware/not-found.js')
+const errorHandlerMiddleware = require('./NodeJSProjects/TaskManager/Middleware/error-Handler.js')
 
 // middleware
+//app.use(express.static('./index.js')) // the main page/dashboard
 app.use(express.json()); // to get the information in the right form
 
 //  routes
-app.get('/hello', (req, res) => {
-    res.send("Task Manager App")
-})
+// app.get('/hello', (req, res) => {
+//     res.send("Task Manager App")
+// })
 
 app.use('/api/v1/tasks', tasks);
 
-app.get('/');
-app.post('/');
-app.get('/:id');
-app.patch('/:id');
-app.delete('/:id');
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+// app.get('/');
+// app.post('/');
+// app.get('/:id');
+// app.patch('/:id'); // patch is used for partial update, while put changes even the structure/the object
+// app.delete('/:id');
 
 
 const port = 3000;
