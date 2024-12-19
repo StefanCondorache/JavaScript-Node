9b520cd282155cd58ac96b7154f0a78a681a7571
StefanCondorache <stefan.condorache@gmail.com>
2024-12-19 14:58:32 +0200

    changed main

--- NodeJSProjects/JobsAPI/main.js
+++ NodeJSProjects/JobsAPI/main.js
@@ -1,6 +1,12 @@
 require('dotenv').config({ path : 'data.env' });
 require('express-async-errors');
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -17,7 +23,16 @@ const jobsRouter = require('./NodeJSProjects/JobsAPI/Routes/jobs.js')
 const notFoundMiddleWare = require('./NodeJSProjects/JobsAPI/Middleware/not-found');
 const errorHanderMiddleware = require('./NodeJSProjects/JobsAPI/Middleware/error-Handler');
 
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+        windowMs: 15 * 60 * 1000,   // 15 minutes
+        max: 100                    // Limit each IP to 100 requests per windowMs 
+    })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
